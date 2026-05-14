9d52348847f061425f76a4a5cbe6a55490dea3a3
Salo018 <salome.rivas@uao.edu.co>
2026-05-13 19:07:51 -0500

    Commit with Kafka running and verify the database

--- kafka/producer.py
+++ kafka/producer.py
@@ -13,7 +13,7 @@ X = df[['GDP', 'Social_Support', 'Life_Expectancy', 'Freedom']]
 y = df['Happiness_Score']
 
 # Mismo split que en training (random_state=42 para que sea el mismo 30%)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=80)
 
 # Crear producer
 producer = KafkaProducer(
